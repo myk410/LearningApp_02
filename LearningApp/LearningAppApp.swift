88df9cbd309b278ef0c909ee88ced71d5156a9f4
myk410 <myk410@gmail.com>
2021-12-03 15:22:02 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by G. Michael Fortin Jr on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
