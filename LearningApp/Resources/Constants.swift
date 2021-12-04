df10bf107bddaf1fa84d10aeb0efab8e26346367
myk410 <myk410@gmail.com>
2021-12-04 12:40:29 -0500

    Added NavigationLinks to ContentView Added navigationViewStyle to HomeView so app doesn't get booted out of ContentDetailView Added ContentDetailView Added Constants.swift file Added AVKit Added Next lesson functionality in ContentModel

---
+++ LearningApp/Resources/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by G. Michael Fortin Jr on 12/4/21.
+//
+
+import Foundation
+
+struct Constants {
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+}
